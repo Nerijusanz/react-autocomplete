0596a20a1e0488ec5af96a741c5105e3fb9c6630
nerijus-ubuntu <nerijusanz@gmail.com>
2019-02-02 13:24:33 +0200

    components/Autocomplete.js created

---
+++ src/components/Autocomplete.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Autocomplete extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Autocomplete</h1>
+      </div>
+    );
+  }
+}
+
+export default Autocomplete;
