8cfc71dcf05d99dca29c9c8d382da592a17a9527
nerijus-ubuntu <nerijusanz@gmail.com>
2019-02-02 15:17:41 +0200

    ./src.components/Autocomplete.js updated. autocomplete development

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,10 +1,84 @@
 import React, { Component } from "react";
+import countries from "../data/countries";
 
 class Autocomplete extends Component {
+  state = {
+    items: [],
+    suggestions: [],
+    data: {
+      autocomplete: ""
+    }
+  };
+
+  componentDidMount = () => {
+    this.initAutocomplete();
+  };
+
+  initAutocomplete = () => {
+    if (countries.length > 0) this.setState({ items: countries });
+  };
+
+  changeHandler = e => {
+    const value = e.target.value;
+
+    let suggestions = [];
+
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, "i");
+
+      suggestions = this.state.items.sort().filter(item => regex.test(item));
+    }
+
+    this.setState({
+      suggestions,
+      data: {
+        ...this.state.data,
+        autocomplete: value
+      }
+    });
+  };
+
+  suggestionSelected = item => {
+    this.setState({
+      suggestions: [],
+      data: {
+        ...this.state.data,
+        autocomplete: item
+      }
+    });
+  };
+
+  renderSuggestions = () => {
+    const { suggestions } = this.state;
+
+    let list = "";
+
+    if (suggestions.length > 0) {
+      list = suggestions.map(suggestion => (
+        <li onClick={() => this.suggestionSelected(suggestion)}>
+          {suggestion}
+        </li>
+      ));
+    }
+
+    return <ul>{list}</ul>;
+  };
+
   render() {
     return (
       <div>
-        <h1>Autocomplete</h1>
+        <h1 className="title">Autocomplete</h1>
+        <div className="autocomplete">
+          <input
+            type="text"
+            name="autocomplete"
+            onChange={this.changeHandler}
+            value={this.state.data.autocomplete}
+          />
+          <div className="autocomplete-suggestion-list">
+            {this.renderSuggestions()}
+          </div>
+        </div>
       </div>
     );
   }
